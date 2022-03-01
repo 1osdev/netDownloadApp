1758b904cb1734796117e823c39de436eed11728
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-03-01 03:25:00 +0300

    project changed #2

--- netDownloadApp/netDownloadApp/Controllers/ViewController.swift
+++ netDownloadApp/netDownloadApp/Controllers/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     let URLString = [
-        "https://i.pinimg.com/originals/df/c0/ec/dfc0ec844718275d1e4ed1a6e56491ef.jpg",
+        "1https://i.pinimg.com/originals/df/c0/ec/dfc0ec844718275d1e4ed1a6e56491ef.jpg",
         "https://kwinsy.com.ua/image/catalog/tovar/kartini/KHO2153.jpg",
         "https://images-na.ssl-images-amazon.com/images/I/A1dRh330y5L.jpg",
         "https://upload.wikimedia.org/wikipedia/commons/1/14/Van_Gogh_-_Blühender_Pfirsichbaum.jpeg",
-        "1https://cp16.nevsepic.com.ua/279/27907/1475831065-vyatskie-zhuravli.jpg"
+        "https://cp16.nevsepic.com.ua/279/27907/1475831065-vyatskie-zhuravli.jpg"
     ]
 
 
@@ -42,10 +42,10 @@ class ViewController: UIViewController {
         let urlString = selectUrlString(number: randomImage())
 
         guard let imageURL = URL(string: urlString) else { return }
-        
+
         let task = URLSession.shared.dataTask(with: imageURL) {
             (data, response, error) in
-            
+
             var displayImage: UIImage?
 
             if let error = error {
@@ -63,11 +63,11 @@ class ViewController: UIViewController {
         }
         task.resume()
     }
-        
-        func randomImage() -> Int {
-            return Int(arc4random_uniform(5))
-        }
-        func selectUrlString(number: Int) -> String {
-            return URLString[number]
-        }
+
+    func randomImage() -> Int {
+        return Int(arc4random_uniform(5))
     }
+    func selectUrlString(number: Int) -> String {
+        return URLString[number]
+    }
+}
